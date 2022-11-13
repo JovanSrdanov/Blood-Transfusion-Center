16ef9fd9e8033560402268b8a6905d61e5f377ac
Strahinja Eraković <karte32114@gmail.com>
2022-11-14 00:17:29 +0100

    Create WebConfiguration.java

---
+++ BackEnd/ISA_2022/src/main/java/groupJASS/ISA_2022/WebConfiguration.java
@@ -0,0 +1,15 @@
+package groupJASS.ISA_2022;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
+
