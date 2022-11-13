3e9257da570d68d2ac3fb1d7430b6c7d75b8f193
stefanapostolovic <93873256+stefanapostolovic@users.noreply.github.com>
2022-11-13 14:20:53 +0100

    added angular materials

--- FrontEnd/ISA/src/app/app.module.ts
+++ FrontEnd/ISA/src/app/app.module.ts
@@ -1,18 +1,19 @@
+import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
