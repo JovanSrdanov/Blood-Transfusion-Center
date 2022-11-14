e56cd54e96821aa79f3fe39008f63b750489c7b3
stefanapostolovic <93873256+stefanapostolovic@users.noreply.github.com>
2022-11-14 13:18:31 +0100

    fixing errors after repackaging

--- FrontEnd/ISA/src/app/features/blood-admin-profile/blood-admin-profile.module.ts
+++ FrontEnd/ISA/src/app/features/blood-admin-profile/blood-admin-profile.module.ts
@@ -1,17 +1,12 @@
+import { BloodAdminProfileComponent } from './blood-admin-profile/blood-admin-profile.component';
 import { MaterialModule } from '../../material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BloodAdminProfileComponent } from './blood-admin-profile/blood-admin-profile.component';
 import { NavbarModule } from '../navbar/navbar.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BloodAdminProfileComponent],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    NavbarModule,
-    FormsModule
-    ]
+  imports: [CommonModule, MaterialModule, NavbarModule, FormsModule],
 })
 export class BloodAdminProfileModule {}
