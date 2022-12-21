2c6df76cd2c746f9aaaf6847b3eb822524c6abd7
strale15 <karte32114@gmail.com>
2022-12-21 23:59:10 +0100

    Sceduling done

--- BackEnd/ISA_2022/src/main/java/groupJASS/ISA_2022/Controller/AppointmentController.java
+++ BackEnd/ISA_2022/src/main/java/groupJASS/ISA_2022/Controller/AppointmentController.java
@@ -1,20 +1,24 @@
 package groupJASS.ISA_2022.Controller;
 
+import groupJASS.ISA_2022.DTO.Appointment.AvailablePredefinedDto;
 import groupJASS.ISA_2022.DTO.Appointment.AvailableSlotsDto;
 import groupJASS.ISA_2022.DTO.Appointment.PredefineAppointmentDto;
+import groupJASS.ISA_2022.Exceptions.BadRequestException;
+import groupJASS.ISA_2022.Model.Account;
 import groupJASS.ISA_2022.Model.Appointment;
+import groupJASS.ISA_2022.Model.AppointmentSchedulingHistory;
 import groupJASS.ISA_2022.Model.DateRange;
 import groupJASS.ISA_2022.Service.Interfaces.IAccountService;
 import groupJASS.ISA_2022.Service.Interfaces.IAppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("appointment")
@@ -29,17 +33,44 @@ public class AppointmentController {
     }
 
     @PostMapping("/available-admin")
-    public ResponseEntity<List<DateRange>> findAll(@RequestBody AvailableSlotsDto dto) {
+    @PreAuthorize("hasRole('STAFF')")
+    public ResponseEntity<List<DateRange>> findAll(@RequestBody AvailableSlotsDto dto, Principal account) {
+        Account a = _accountService.findAccountByEmail(account.getName());
         var res = (List<DateRange>) _appointmentService.findFreeSlotsForStaffIds(dto.getStaffIds(), dto.getDate(),
                 dto.getDuration());
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
     @PostMapping("/predefine")
-    public ResponseEntity<Appointment> predefine(@RequestBody PredefineAppointmentDto dto) {
-        var res = (Appointment) _appointmentService.predefine(dto.getDateRange(), dto.getStaffIds());
+    @PreAuthorize("hasRole('STAFF')")
+    public ResponseEntity<?> predefine(@RequestBody PredefineAppointmentDto dto, Principal account) {
+        Account a = _accountService.findAccountByEmail(account.getName());
+        Appointment res = null;
+        try {
+            res = _appointmentService.predefine(dto.getDateRange(), dto.getStaffIds(), a.getPersonId());
+        }
+        catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
+
+    @GetMapping("/available-donor/{cid}")
+    @PreAuthorize("hasRole('BLOOD_DONOR')")
+    public ResponseEntity<List<AvailablePredefinedDto>> findAvailableAppointmentsForDonor(@PathVariable UUID cid, Principal account) {
+        Account a = _accountService.findAccountByEmail(account.getName());
+        var res = (List<AvailablePredefinedDto>)_appointmentService.findAvailableAppointmentsForDonor(a.getPersonId(), cid);
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @PostMapping("/schedulePredefine/{appid}")
+    @PreAuthorize("hasRole('BLOOD_DONOR')")
+    public ResponseEntity<AppointmentSchedulingHistory> scheduleAppointment(@PathVariable UUID appid, Principal account) {
+        Account a = _accountService.findAccountByEmail(account.getName());
+        var res = (AppointmentSchedulingHistory)_appointmentService.scheduleAppointment(a.getPersonId(), appid);
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
 
 
 }
--- BackEnd/ISA_2022/src/main/java/groupJASS/ISA_2022/Model/AppointmentSchedulingHistory.java
+++ BackEnd/ISA_2022/src/main/java/groupJASS/ISA_2022/Model/AppointmentSchedulingHistory.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -20,12 +20,12 @@ public class AppointmentSchedulingHistory {
     private String QRcode;
 
     @Column(nullable = false)
-    private LocalDate issuingDate;
+    private LocalDateTime issuingDate;
 
     @Column(nullable = false)
     private AppointmentSchedulingConfirmationStatus status;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "appointment_id")
     private Appointment appointment;
 
@@ -35,4 +35,17 @@ public class AppointmentSchedulingHistory {
 
     @OneToOne(mappedBy = "appointmentSchedulingHistory")
     private AppointmentReport appointmentReport;
+
+    public AppointmentSchedulingHistory(UUID id, String QRcode, LocalDateTime issuingDate,
+                                        AppointmentSchedulingConfirmationStatus status,
+                                        Appointment appointment, BloodDonor bloodDonor,
+                                        AppointmentReport appointmentReport) {
+        this.id = id;
+        this.QRcode = QRcode;
+        this.issuingDate = issuingDate;
+        this.status = status;
+        this.appointment = appointment;
+        this.bloodDonor = bloodDonor;
+        this.appointmentReport = appointmentReport;
+    }
 }
