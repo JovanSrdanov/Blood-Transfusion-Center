59f1387fb77d6dc0e6d1d67854ae2a7e7309388c
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2023-01-23 16:32:56 +0100

    Rabbit MQ works

--- ExternalHospital/src/main/java/ExternalHospital/ExternalHospital/Controllers/GPSController.java
+++ ExternalHospital/src/main/java/ExternalHospital/ExternalHospital/Controllers/GPSController.java
@@ -1,5 +1,7 @@
 package ExternalHospital.ExternalHospital.Controllers;
 
+import ExternalHospital.ExternalHospital.Utilities.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("gps")
 public class GPSController {
+    @Autowired
+    private Producer producer;
+
 
 
     @GetMapping("test")
     public ResponseEntity<?> findAll() {
+        producer.sendTo("spring-boot1", "cao stefane");
         return new ResponseEntity<>("GPS CONTROLLER WORKS", HttpStatus.OK);
     }
 }
--- ExternalHospital/src/main/java/ExternalHospital/ExternalHospital/ExternalHospitalApplication.java
+++ ExternalHospital/src/main/java/ExternalHospital/ExternalHospital/ExternalHospitalApplication.java
@@ -1,9 +1,18 @@
 package ExternalHospital.ExternalHospital;
-
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+
+
 
 @SpringBootApplication
 @EnableAsync
@@ -13,6 +22,46 @@ public class ExternalHospitalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExternalHospitalApplication.class, args);
 		System.out.println("--- EXTERNAL HOSPITAL ---");
+	}
+
+	@Value("${myqueue}")
+	String queue;
+
+	@Value("${myqueue2}")
+	String queue2;
+
+	@Value("${myexchange}")
+	String exchange;
+
+	@Value("${routingkey}")
+	String routingkey;
+
+
+	@Bean
+	Queue queue() {
+		return new Queue(queue, true);
+	}
+
+	@Bean
+	Queue queue2() {
+		return new Queue(queue2, true);
+	}
+
+	@Bean
+	DirectExchange exchange() {
+		return new DirectExchange(exchange);
+	}
+
+	@Bean
+	Binding binding(Queue queue2, DirectExchange exchange) {
+		return BindingBuilder.bind(queue2).to(exchange).with(routingkey);
+	}
+
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+		return connectionFactory;
 	}
 
 }
