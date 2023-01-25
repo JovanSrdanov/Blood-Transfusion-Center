85eac8f6b13a4c360bbcb877b763c18a28ef790c
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2023-01-25 13:10:12 +0100

    Cache account done

--- BackEnd/ISA_2022/src/main/java/groupJASS/ISA_2022/Service/Interfaces/IAccountService.java
+++ BackEnd/ISA_2022/src/main/java/groupJASS/ISA_2022/Service/Interfaces/IAccountService.java
@@ -3,17 +3,22 @@ package groupJASS.ISA_2022.Service.Interfaces;
 import groupJASS.ISA_2022.DTO.Account.AccountDTO;
 import groupJASS.ISA_2022.DTO.Account.ActivateAccountDTO;
 import groupJASS.ISA_2022.Model.Account;
-import groupJASS.ISA_2022.Model.BloodDonor;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.UUID;
 
 public interface IAccountService extends ICrudService<Account> {
 
-    public void  updatePassword(String email, String newPassword);
+    void updatePassword(String email, String newPassword);
 
     Account registerAccount(AccountDTO accountDto, String roleName, UUID personId);
+
     boolean checkEmailAvailability(String email);
+
     Account findAccountByPersonId(UUID personId);
+
+    @Cacheable("findAccountByEmail")
     Account findAccountByEmail(String email);
+
     Account activateAccount(ActivateAccountDTO activateAccountDTO);
 }
